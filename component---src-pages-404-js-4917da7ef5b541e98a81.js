cb52c64e3654ebd9b562b237f052f2d7e1fcfdc2
soobing <qls0147@naver.com>
2022-02-20 19:42:15 +0900

    Updates

---
+++ component---src-pages-404-js-4917da7ef5b541e98a81.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkzoomkoding_com=self.webpackChunkzoomkoding_com||[]).push([[883],{6810:function(e,t,n){n.r(t);var o=n(7294),u=n(1280),l=n(5150);t.default=function(){return o.createElement(u.Z,null,o.createElement(l.Z,{title:"404: Not found"}),o.createElement("h1",null,"404: Not Found"),o.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4917da7ef5b541e98a81.js.map
--- src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-export const getUniqueCategories = (posts) => {
-  const categorySet = new Set();
-  posts.forEach(({ categories }) => categories.forEach((category) => categorySet.add(category)));
-  return [...categorySet].sort((a, b) => {
-    if (a === 'featured') return -1;
-    if (b === 'featured') return 1;
-    return 0;
-  });
-};
