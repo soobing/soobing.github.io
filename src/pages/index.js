3a43760faea133d46cba217439caf6e1e0d204a8
soobing <qls0147@naver.com>
2022-07-23 17:47:44 +0900

    refactor: 홈에서 post 영역 삭제

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,28 +5,14 @@ import Seo from '../components/seo';
 import Bio from '../components/bio';
 import Post from '../models/post';
 
-import { getUniqueCategories } from '../utils/helpers';
-import PostTabs from '../components/post-tabs';
-
 function HomePage({ data }) {
   const posts = data.allMarkdownRemark.edges.map(({ node }) => new Post(node));
   const { author, language } = data.site.siteMetadata;
-  const categories = ['All', ...getUniqueCategories(posts)];
-  const featuredTabIndex = categories.findIndex((category) => category === 'featured');
-  const [tabIndex, setTabIndex] = useState(featuredTabIndex === -1 ? 0 : featuredTabIndex);
-  const onTabIndexChange = useCallback((e, value) => setTabIndex(value), []);
 
   return (
     <Layout>
       <Seo title="Home" />
       <Bio author={author} language={language} />
-      <PostTabs
-        posts={posts}
-        onChange={onTabIndexChange}
-        tabs={categories}
-        tabIndex={tabIndex}
-        showMoreButton
-      />
     </Layout>
   );
 }
