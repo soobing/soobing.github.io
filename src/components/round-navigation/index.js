7f038726c6e7e08695d73c6b5bb2e09716aa42d4
soobing <qls0147@naver.com>
2022-07-24 15:13:05 +0900

    feat(round-navigation): current-line mouse move

--- src/components/round-navigation/index.js
+++ src/components/round-navigation/index.js
@@ -1,11 +1,34 @@
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'gatsby';
-import React from 'react';
 import './style.scss';
 
 function RoundNavigation() {
+  const [angle, setAngle] = useState(0);
+  const navRef = useRef(null);
+
+  const getAngle = ({ x: x1, y: y1 }, { x: x2, y: y2 }) => {
+    const dy = y2 - y1;
+    const dx = x2 - x1;
+    return Math.atan2(dy, dx) * (180 / Math.PI);
+  };
+
+  useEffect(() => {
+    const rect = navRef.current.getBoundingClientRect();
+    const centerPos = { x: rect.x + rect.width / 2, y: rect.y + rect.height / 2 };
+    const onMouseMove = (e) => {
+      setAngle(getAngle({ x: e.clientX, y: e.clientY }, centerPos) - 90);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    return () => {
+      document.removeEventListener('mousemove', onMouseMove);
+    };
+  }, []);
   return (
-    <nav className="round-navigation__wrapper">
-      <span className="current-line" />
+    <nav ref={navRef} className="round-navigation__wrapper">
+      <span
+        className="current-line"
+        style={{ transform: `translate(-50%, 0%) rotate(${angle}deg)` }}
+      />
       <ul>
         <li className="link link--12">
           <Link to="/">
