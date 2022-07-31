9bb52390cfd557aec2daf693aa2611caebc6f62c
soobing <qls0147@naver.com>
2022-07-31 17:34:08 +0900

    fix(nav): isActive

--- src/components/horizontal-navigation/index.js
+++ src/components/horizontal-navigation/index.js
@@ -9,16 +9,16 @@ function HorizontalNavigation() {
     { path: '/posts', name: 'POSTS' },
   ];
 
-  const refineSlash = (value = '') => value.replace('/', '');
-  const isCurrentPath = (path) => refineSlash(path) === refineSlash(window?.location.pathname);
-
+  const isActive = ({ isCurrent }) => {
+    return isCurrent ? { className: 'active' } : {};
+  };
   return (
     <nav className="horizontal-navigation">
       <hr className="line" />
       <ul className="list">
         {routes.map(({ path, name }) => (
           <li key={`nav-${path}`}>
-            <Link to={path} className={isCurrentPath(path) ? 'active' : ''}>
+            <Link to={path} getProps={isActive}>
               <span className="label">{name}</span>
             </Link>
           </li>
