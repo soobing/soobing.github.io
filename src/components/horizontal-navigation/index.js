96a5fd1c24fba63e8e85286558a37aa31a398105
soobing <qls0147@naver.com>
2022-07-31 16:57:32 +0900

    fix(hor-nav): active

--- src/components/horizontal-navigation/index.js
+++ src/components/horizontal-navigation/index.js
@@ -8,13 +8,17 @@ function HorizontalNavigation() {
     { path: '/about', name: 'ABOUT' },
     { path: '/posts', name: 'POSTS' },
   ];
+
+  const refineSlash = (value = '') => value.replace('/', '');
+  const isCurrentPath = (path) => refineSlash(path) === refineSlash(window?.location.pathname);
+
   return (
     <nav className="horizontal-navigation">
       <hr className="line" />
       <ul className="list">
         {routes.map(({ path, name }) => (
-          <li>
-            <Link to={path} className="active">
+          <li key={`nav-${path}`}>
+            <Link to={path} className={isCurrentPath(path) ? 'active' : ''}>
               <span className="label">{name}</span>
             </Link>
           </li>
